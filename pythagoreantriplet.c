58166b2820807cd5a4bd5f8db59ce7b053171aac
Aash-Sasha <physicswithaash@gmail.com>
2016-10-18 15:36:52 +0530

    Update pythagoreantriplet.c

--- pythagoreantriplet.c
+++ pythagoreantriplet.c
@@ -1,1 +1,45 @@
-a
+#include <stdio.h>
+#include <conio.h>
+
+//creator aash
+//facebook page
+//version=1.0
+//creator will add comment for proper explaination soon.
+
+void main()
+{
+	int a, b, c,s1,s2,s3,m1,m2,m3;
+	printf("input a \n");
+	scanf_s("%d", &a);
+	printf("input b \n");
+	scanf_s("%d", &b);
+	printf("input c \n");
+	scanf_s("%d", &c);
+	
+
+	s1 = a, s2 = b, s3 = c;
+	if (s2>s1)
+		s1 = b, s2 = a, s3 = c; 
+	
+	if (s3>s1)
+		s1 = c, s2 = a, s3 = b; 
+
+	printf("%d \n", s1);
+	
+	m1 = s1*s1;
+	m2 = s2*s2;
+	m3 = s3*s3;
+
+
+	if (m1 == m2 + m3)
+	{
+		printf("y");
+	}
+	else
+	{
+		printf("n");
+	}
+	
+	_getch();
+}
+
